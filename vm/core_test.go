cc768ad669e66e39ce799a2e9888ccfbe9ec4da0
He Liu <liulonnie@gmail.com>
2014-02-22 00:45:32 -0800

    checking instruction used

--- vm/core_test.go
+++ vm/core_test.go
@@ -33,12 +33,12 @@ func TestHelloWorld(t *testing.T) {
 	/*
 			add $1, $0, $0		; init counter
 		loop:
-		wait:
 			lbu $2, $1[0x2000] 	; load byte
 			beq $2, $0, end 	; +5
+		wait:
 			lbu $3, $0[0x5]    	; is output ready?
-			bne $3, $0, wait 	; -4
-			sb $2, [0x5]  		; output byte
+			bne $3, $0, wait 	; -2
+			sb $2, $0[0x5]		; output byte
 			addi $1, $1, 1		; increase counter
 			j loop 				; -7
 		end:
@@ -49,14 +49,18 @@ func TestHelloWorld(t *testing.T) {
 	w(Iinst(OpLbu, 1, 2, 0x2000))  // 004
 	w(Iinst(OpBeq, 2, 0, 0x0005))  // 008
 	w(Iinst(OpLbu, 0, 3, 0x0005))  // 00c
-	w(Iinst(OpBne, 3, 0, 0xfffc))  // 010
+	w(Iinst(OpBne, 3, 0, 0xfffe))  // 010
 	w(Iinst(OpSb, 0, 2, 0x0005))   // 014
 	w(Iinst(OpAddi, 1, 1, 0x0001)) // 018
 	w(Jinst(-7))                   // 01c
 	w(Iinst(OpSb, 0, 0, 0x0004))   // 020
 
 	c.SetPC(PageStart(1))
-	c.Run(1000)
+	left := c.Run(1000)
+
+	if left < 850 {
+		t.Fail()
+	}
 
 	if !c.Halt || c.HaltValue != 0 || c.AddrError {
 		t.Fail()
